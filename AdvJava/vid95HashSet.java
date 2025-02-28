9c79dd6f5ebc6024892f6f7ec386304f59e2f70e
Umar Hayat <umarhayat9718@gmail.com>
2025-02-28 23:20:09 +0530

    28/02 Learning

---
+++ AdvJava/vid95HashSet.java
@@ -0,0 +1,15 @@
+package AdvJava;
+
+import java.util.HashSet;
+
+public class vid95HashSet {
+    public static void main(String[] args) {
+        HashSet<Integer> HashOne = new HashSet<>();
+        HashOne.add(45);
+        HashOne.add(46);
+        HashOne.add(47);
+        HashOne.add(48);
+        HashOne.add(49);
+        System.out.println(HashOne);
+    }
+}
